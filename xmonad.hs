70567d028cbf9a82a145dbea4eb19e35e02f8947
Risto Helinko <risto.helinko@gmail.com>
2017-10-01 00:54:48 +0300

    switched the terminal to st with tmux

--- xmonad.hs
+++ xmonad.hs
@@ -14,7 +14,7 @@ main = do
                 { ppOutput = hPutStrLn xmproc
                 , ppTitle = xmobarColor "green" "" . shorten 50
                 }
-    , terminal = "lxterminal"
+    , terminal = "st -e tmux"
     --    , modMask = mod4Mask     -- Rebind Mod to the Windows key
     }
 
