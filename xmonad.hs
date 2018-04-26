7f4e7fe0f5c2e80da07655cf825b46c39f010c69
Risto Helinko <risto.helinko@relexsolutions.com>
2018-04-26 16:16:12 +0300

    WIP, not tested

--- xmonad.hs
+++ xmonad.hs
@@ -4,7 +4,59 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
 import System.IO
+import XMonad.Actions.TopicSpace
 
+browserCmd :: String
+browserCmd = "firefox"
+
+myTopics :: [Topic]
+myTopics =
+  [ "dashboard" -- the first one
+  , "flowdock", "wire", "spotify", "web"
+  , "emacs", "mordor", "system-management"
+  ]
+  
+myTopicConfig :: TopicConfig
+myTopicConfig = def
+  { topicDirs = M.fromList $
+      [ ("conf", "w/conf")
+      , ("dashboard", "Desktop")
+      , ("mordor", "w/mordor")
+      , ("darcs", "w/dev-haskell/darcs")
+      , ("haskell", "w/dev-haskell")
+      , ("xmonad", "w/dev-haskell/xmonad")
+      , ("tools", "w/tools")
+      , ("movie", "Movies")
+      , ("talk", "w/talks")
+      , ("music", "Music")
+      , ("documents", "w/documents")
+      , ("pdf", "w/documents")
+      ]
+  , defaultTopicAction = const $ spawnShell >*> 2
+  , defaultTopic = "dashboard"
+  , topicActions = M.fromList $
+      [ ("flowdock", spawn browserCmd)
+      , ("spotify", spawn "spotify")
+      , ("dashboard",  spawnShell)
+      , ("web",        spawn browserCmd)
+      ]
+  }
+
+spawnShell :: X ()
+spawnShell = currentTopicDir myTopicConfig >>= spawnShellIn
+
+spawnShellIn :: Dir -> X ()
+spawnShellIn dir = spawn $ "urxvt '(cd ''" ++ dir ++ "'' && " ++ myShell ++ " )'"
+
+goto :: Topic -> X ()
+goto = switchTopic myTopicConfig
+
+promptedGoto :: X ()
+promptedGoto = workspacePrompt myXPConfig goto
+
+promptedShift :: X ()
+promptedShift = workspacePrompt myXPConfig $ windows . W.shift
+ 
 main = do
   xmproc <- spawnPipe "xmobar"
   xmonad $ docks defaultConfig
